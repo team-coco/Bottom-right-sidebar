f7053fa1dbaf32462cfca306ac6d7eed0a909603
Carlton Caven <carlcaven@gmail.com>
2018-02-28 15:55:40 -0800

    add static images

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -25,34 +25,64 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="page">
         <h1 className="rightsb_header">Things to Consider</h1>
         <h2 className="rightsb_subheader">People Also Viewed</h2>
-        <li className="rightsb_listitem">img Place 1</li>
-        <li className="rightsb_listitem">img Place 2</li>
-        <li className="rightsb_listitem">img Place 3</li>
+        <p className="rightsb_listitem">img Place 1</p>
+        <p className="rightsb_listitem">img Place 2</p>
+        <p className="rightsb_listitem">img Place 3</p>
         <h2 className="rightsb_subheader">Other Places Nearby</h2>
-        <li className="rightsb_listitem">
+        <p className="rightsb_listitem">
           Find more Places Near {this.state.business[0].name}
-        </li>
-        <li className="rightsb_listitem">
+        </p>
+        <p className="rightsb_listitem">
           Find more Burgers Near {this.state.business[0].name}
-        </li>
-        <li className="rightsb_listitem">
+        </p>
+        <p className="rightsb_listitem">
           Find more Sandwiches Near {this.state.business[0].name}
-        </li>
+        </p>
         <h2 className="rightsb_subheader">Browse Nearby</h2>
-        <li className="rightsb_listitem">Restaurants</li>
-        <li className="rightsb_listitem">Nightlife</li>
-        <li className="rightsb_listitem">Shopping</li>
-        <li className="rightsb_listitem">...Show All</li>
+        <p className="rightsb_listitem">
+          {" "}
+          <img src={"./icons/icon_restaurant.png"} />Restaurants
+        </p>
+        <p className="rightsb_listitem">
+          {" "}
+          <img src={"./icons/icon_nightlife.png"} />Nightlife
+        </p>
+        <p className="rightsb_listitem">
+          {" "}
+          <img src={"./icons/icon_shopping.jpeg"} />Shopping
+        </p>
+        <p className="rightsb_listitem">
+          <img src={"./icons/icon_ellipsis.png"} />Show All
+        </p>
         <h2 className="rightsb_subheader">
           Dining in {this.state.business[0].city}
         </h2>
-        <li className="rightsb_listitem">Restaurants</li>
-        <li className="rightsb_listitem">Nightlife</li>
-        <li className="rightsb_listitem">Shopping</li>
-        <li className="rightsb_listitem">...Show All</li>
+        <p className="icon">
+          <img src={"./icons/icon_calendar.png"} />
+        </p>
+        <p className="rightsb_listitem">Search for Reservations</p>
+        <p className="rightsb_listitem">
+          <img src={"./icons/icon_magglass.jpeg"} />
+          Book a table in {this.state.business[0].city}
+        </p>
+        <h2 className="rightsb_subheader">
+          Best of {this.state.business[0].city}
+        </h2>
+        <p className="rightsb_listitem">
+          Things to do in {this.state.business[0].city}
+        </p>
+        <h2 className="rightsb_subheader">
+          People found {this.state.business[0].name} by searching for...
+        </h2>
+        <p className="rightsb_listitem">
+          "Food item" {this.state.business[0].city}
+        </p>
+        <h2 className="rightsb_subheader">Near Me</h2>
+        <p className="rightsb_listitem">"Food item"</p>
+        <p className="rightsb_listitem">"Food item2"</p>
       </div>
     );
   }
