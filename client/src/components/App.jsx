dde535cfab27dd292cb69382a7989c0f7ecc8ec9
Carlton Caven <carlcaven@gmail.com>
2018-02-28 13:46:13 -0800

    state set and rendering accurate data to client

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,25 +6,21 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      business: []
+      business: [{}]
     };
   }
   componentDidMount() {
     axios
       .get("/repos")
       .then(response => {
-        this.setState({ business: response });
-        console.log(response);
+        this.setState({ business: response.data });
+        console.log(this.state.business[0].city);
+        // we are getting city in the console so accessing is correct
+        console.log(response, "THIS IS RESPONSE AND SHOULD BE ON WEBPAGE");
       })
       .catch(err => {
         console.log(err);
       });
-  }
-
-  getLocation(location) {
-    if (location) {
-      return <p>Location: {location}</p>;
-    }
   }
 
   render() {
@@ -36,15 +32,23 @@ class App extends React.Component {
         <li className="rightsb_listitem">img Place 2</li>
         <li className="rightsb_listitem">img Place 3</li>
         <h2 className="rightsb_subheader">Other Places Nearby</h2>
-        <li className="rightsb_listitem" />
-        <li className="rightsb_listitem" />
-        <li className="rightsb_listitem" />
+        <li className="rightsb_listitem">
+          Find more Places Near {this.state.business[0].name}
+        </li>
+        <li className="rightsb_listitem">
+          Find more Burgers Near {this.state.business[0].name}
+        </li>
+        <li className="rightsb_listitem">
+          Find more Sandwiches Near {this.state.business[0].name}
+        </li>
         <h2 className="rightsb_subheader">Browse Nearby</h2>
         <li className="rightsb_listitem">Restaurants</li>
         <li className="rightsb_listitem">Nightlife</li>
         <li className="rightsb_listitem">Shopping</li>
         <li className="rightsb_listitem">...Show All</li>
-        <h2 className="rightsb_subheader">Dining in San Francisco</h2>
+        <h2 className="rightsb_subheader">
+          Dining in {this.state.business[0].city}
+        </h2>
         <li className="rightsb_listitem">Restaurants</li>
         <li className="rightsb_listitem">Nightlife</li>
         <li className="rightsb_listitem">Shopping</li>
--- server/index.js
+++ server/index.js
@@ -22,13 +22,14 @@ connection.connect(function(err) {
 
 app.get("/repos", function(req, res) {
   // connection.connect();
-  var q = 'SELECT * FROM business WHERE city="Toronto"';
+  var q = 'SELECT * FROM business WHERE city="Toronto" LIMIT 1';
   // We will agree on roughly ~10 business ID's to test
   // id business city needs to be dynamic based on the business ID
   // query needs to reflect location and cuisine
   connection.query(q, function(err, rows, fields) {
     if (err) throw err;
-    console.log(rows);
+    console.log(rows, "hi im rows");
+    // console.log shows up in node
     res.send(rows);
   });
 });
